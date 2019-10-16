437115fe91e578aedcdb3a575bc163cc34954e27
Berislav <berislav.hunjadi@gmail.com>
2019-10-16 19:53:30 +0000

    First version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,208 @@
-export function processFilters(a: object): object {
-    return {};
+// const COMPARISON_OPERATORS = ['$eq', '$gt', '$gte', '$in', '$lt', '$lte', '$ne', '$nin'];
+
+// const LOGICAL_OPERATORS = ['$and', '$not', '$nor', '$or'];
+
+const OPERATORS_WITH_ARRAY = ['$and', '$or', '$nor'];
+
+const SPECIAL_TOP_LEVEL_OPERATORS = ['$expr', '$text', '$where', '$comment'];
+
+interface ErrorHandlingConfig {
+    throw?: boolean;
+    onError?(error: ErrorCallback): void;
+}
+
+type FieldOperators = { [key: string]: boolean };
+
+type TopLevelOperators = { [key: string]: boolean };
+
+interface FieldObject {
+    field: string;
+    allowed?: boolean;
+    operators?: FieldOperators;
+}
+
+interface FieldObjectExtension {
+    allowed: boolean;
+}
+
+type Field = string | FieldObject;
+
+type Query = { [key: string]: any };
+
+interface ErrorContext {
+    type: string;
+    name: string;
+    value: any;
+    definition?: FieldObject;
+    operators?: string[];
+}
+
+type ErrorCallback = {
+    config: FiltersConfig;
+    message: string;
+    context: ErrorContext;
+};
+
+interface FiltersConfig {
+    allowedFields?: Field[];
+    topLevelOperators?: TopLevelOperators;
+    errorHandling?: ErrorHandlingConfig;
+}
+
+const DEFAULT_CONFIG: FiltersConfig = {
+    allowedFields: [],
+    errorHandling: { throw: true },
+    topLevelOperators: {},
+};
+
+let defaultConfig = DEFAULT_CONFIG;
+
+function isObject(value: any): boolean {
+    return typeof value === 'object' && value !== null;
+}
+
+function isArray(value: any): boolean {
+    return isObject(value) && value.constructor === Array;
+}
+
+function handleError(config: FiltersConfig, message: string, context: ErrorContext): void {
+    if (config.errorHandling.throw) {
+        throw new Error(message);
+    } else if (config.errorHandling.onError) {
+        config.errorHandling.onError({
+            config,
+            message,
+            context,
+        });
+    }
+}
+
+function normalizeField(field: Field, extension: FieldObjectExtension): FieldObject {
+    if (typeof field === 'string') {
+        return {
+            field,
+            ...extension,
+        };
+    }
+
+    return {
+        ...field,
+        ...extension,
+    };
+}
+
+function normalizeFields(fields: Field[], extension: FieldObjectExtension): FieldObject[] {
+    return fields.map(field => normalizeField(field, extension));
+}
+
+function isOperatorExpression(object: any): boolean {
+    if (isObject(object)) {
+        const keys = Object.keys(object);
+        return keys.every(key => key.startsWith('$'));
+    }
+    return false;
+}
+
+function getFieldDef(config: FiltersConfig, name: string): FieldObject {
+    return (config.allowedFields as FieldObject[]).find(f => f.field === name);
+}
+
+function isFieldAllowed(name: string, value: any, config: FiltersConfig): boolean {
+    const fieldDefinition = getFieldDef(config, name);
+    if (fieldDefinition && fieldDefinition.allowed) {
+        if (!fieldDefinition.operators) {
+            return true;
+        }
+
+        const normalizedValue = isOperatorExpression(value) ? value : { $eq: value };
+        const operators = Object.keys(normalizedValue);
+
+        // handle $not
+        if (operators.length === 1 && operators[0] === '$not') {
+            return fieldDefinition.operators.$not && isFieldAllowed(name, value.$not, config);
+        }
+        // handle $elemMatch
+        if (operators.length === 1 && operators[0] === '$elemMatch') {
+            if (fieldDefinition.operators.$elemMatch) {
+                const elemMatchValue = value.$elemMatch;
+                if (isOperatorExpression(elemMatchValue)) {
+                    return isFieldAllowed(name, elemMatchValue, config);
+                }
+
+                // it is elemMatch for object
+                return Object.keys(elemMatchValue).every(key => {
+                    const elemMatchObjectValue = elemMatchValue[key];
+                    const fieldName = `${fieldDefinition.field}.${key}`;
+                    // const nestedDef = getFieldDef(config, fieldName);
+                    return isFieldAllowed(fieldName, elemMatchObjectValue, config);
+                });
+            }
+        }
+
+        const forbiddenOperators = operators.filter((operator: string) => !fieldDefinition.operators[operator]);
+        if (forbiddenOperators.length === 0) {
+            return true;
+        }
+
+        handleError(config, `Operator(s) ${forbiddenOperators.join(', ')} for field ${name} are not allowed.`, {
+            definition: fieldDefinition,
+            type: 'operator',
+            operators: forbiddenOperators,
+            name,
+            value,
+        });
+    }
+
+    handleError(config, `Field ${name} not allowed by schema.`, {
+        name,
+        value,
+        type: 'field',
+    });
+
+    return false;
+}
+
+function getQuery(query: Query, { config, level }: { config: FiltersConfig; level: number }): Query {
+    const keys = Object.keys(query);
+    const cleanedFilters = {} as Query;
+
+    keys.forEach(key => {
+        const value = query[key] as any;
+        if (OPERATORS_WITH_ARRAY.indexOf(key) > -1) {
+            if (!isArray(value)) {
+                throw new Error(`Expected array value for operator ${key}, got ${typeof value}`);
+            }
+
+            cleanedFilters[key] = value.map((v: Query) => getQuery(v, { config, level: level + 1 }));
+        } else if (SPECIAL_TOP_LEVEL_OPERATORS.indexOf(key) > -1) {
+            if (level === 0 && config.topLevelOperators[key]) {
+                cleanedFilters[key] = value;
+                return;
+            }
+            handleError(config, `Operator ${key} not allowed.`, {
+                type: 'operator',
+                name: key,
+                value,
+            });
+        } else if (isFieldAllowed(key, value, config)) {
+            cleanedFilters[key] = value;
+        }
+    });
+
+    return cleanedFilters;
+}
+
+export function processQuery(query: Query, config: FiltersConfig): Query {
+    if (!isObject(query)) {
+        throw new Error('Query must be an object');
+    }
+
+    config = { ...defaultConfig, ...config };
+    config.allowedFields = normalizeFields(config.allowedFields, { allowed: true });
+
+    return getQuery(query, { config, level: 0 });
+}
+
+export function setDefaultConfig(defaults: FiltersConfig): void {
+    defaultConfig = { ...DEFAULT_CONFIG, ...defaults };
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,416 @@
+import {stub} from 'sinon';
 import {expect} from 'chai';
-import {processFilters} from '../src/index';
+import {processQuery, setDefaultConfig} from '../src/index';
 
-describe('processFilters', () => {
-    it('processes', () => {
-        expect(processFilters({})).to.eql({});
+describe('processFilters - cleaning', () => {
+    before(() => {
+        setDefaultConfig({
+            errorHandling: {
+                throw: false,
+            },
+        });
+    });
+
+    describe('basic', () => {
+        it('works with simple', () => {
+            expect(processQuery({
+                age: 18,
+            }, {
+                allowedFields: ['age'],
+                errorHandling: {throw: false},
+            })).to.eql({
+                age: 18,
+            });
+        });
+    
+        it('works with explicit operator', () => {
+            expect(processQuery({
+                age: {$gt: 18},
+            }, {
+                allowedFields: ['age'],
+            })).to.eql({
+                age: {$gt: 18},
+            });
+        });
+    
+        it('works with multiple operators', () => {
+            expect(processQuery({
+                age: {$gt: 18, $lt: 50},
+            }, {
+                allowedFields: ['age'],
+            })).to.eql({
+                age: {$gt: 18, $lt: 50},
+            });
+        });
+    
+        it('removes forbidden field', () => {
+            expect(processQuery({
+                age: 18,
+            }, {
+                allowedFields: [],
+            })).to.eql({});
+        });
+    
+        it('works with logical $and', () => {
+            expect(processQuery({
+                $and: [{age: 18}],
+            }, {
+                allowedFields: ['age'],
+            })).to.eql({$and: [{age: 18}]});
+        });
+    });
+
+    describe('explicit operator permissions', () => {
+        it('works with $eq allowed', () => {
+            expect(processQuery({age: 18}, {
+                allowedFields: [{
+                    field: 'age',
+                    operators: {
+                        $eq: true,
+                    },
+                }],
+            })).to.eql({age: 18});
+        });
+
+        it('works with $eq forbidden', () => {
+            expect(processQuery({age: 18}, {
+                allowedFields: [{
+                    field: 'age',
+                    operators: {
+                        $eq: false,
+                    },
+                }],
+            })).to.eql({});
+        });
+
+        it('works with $not, $eq allowed', () => {
+            expect(processQuery({age: {$not: {$eq: 18}}}, {
+                allowedFields: [{
+                    field: 'age',
+                    operators: {
+                        $not: true,
+                        $eq: true,
+                    },
+                }],
+            })).to.eql({age: {$not: {$eq: 18}}});
+        });
+
+        it('works with $not, but $eq forbidden', () => {
+            expect(processQuery({age: {$not: {$eq: 18}}}, {
+                allowedFields: [{
+                    field: 'age',
+                    operators: {
+                        $not: true,
+                        $eq: false,
+                    },
+                }],
+            })).to.eql({});
+        });
+
+        it('filters with $not forbidden, but $eq allowed', () => {
+            expect(processQuery({age: {$not: {$eq: 18}}}, {
+                allowedFields: [{
+                    field: 'age',
+                    operators: {
+                        $not: false,
+                        $eq: true,
+                    },
+                }],
+            })).to.eql({});
+        });
+    });
+
+    describe('$elemMatch', () => {
+        it('filters $elemMatch', () => {
+            expect(processQuery({results: {$elemMatch: {$gt: 30, $lt: 50}}}, {
+                allowedFields: [{
+                    field: 'results',
+                    operators: {
+                        $elemMatch: false,
+                    },
+                }],
+            })).to.eql({});
+        });
+
+        it('filters $elemMatch - no additional operators', () => {
+            expect(processQuery({results: {$elemMatch: {$gt: 30, $lt: 50}}}, {
+                allowedFields: [{
+                    field: 'results',
+                    operators: {
+                        $elemMatch: true,
+                    },
+                }],
+            })).to.eql({});
+        });
+
+        it('allows $elemMatch', () => {
+            expect(processQuery({results: {$elemMatch: {$gt: 30, $lt: 50}}}, {
+                allowedFields: [{
+                    field: 'results',
+                    operators: {
+                        $elemMatch: true,
+                        $gt: true,
+                        $lt: true,
+                    },
+                }],
+            })).to.eql({results: {$elemMatch: {$gt: 30, $lt: 50}}});
+        });
+
+        const NESTED_ELEM_MATCH = {
+            results: {
+                $elemMatch: {
+                    rate: 1,
+                },
+            },
+        };
+
+        it('filters nested $elemMatch', () => {
+            expect(processQuery(NESTED_ELEM_MATCH, {
+                allowedFields: [{
+                    field: 'results',
+                    operators: {
+                        $elemMatch: true,
+                    },
+                }],
+            })).to.eql({});
+        });
+
+        it('filters nested $elemMatch', () => {
+            expect(processQuery(NESTED_ELEM_MATCH, {
+                allowedFields: [{
+                    field: 'results',
+                    operators: {
+                        $elemMatch: true,
+                    },
+                }, {
+                    field: 'results.rate',
+                    operators: {
+                        $eq: true,
+                    },
+                }],
+            })).to.eql(NESTED_ELEM_MATCH);
+        });
+    });
+
+    describe('nested fields', () => {
+        const NESTED_FIELDS = {
+            'emails.sent': {$gt: new Date()},
+        };
+
+        it('filters nested fields', () => {
+            expect(processQuery(NESTED_FIELDS, {
+                allowedFields: [{
+                    field: 'emails.sent',
+                    operators: {
+                        $eq: true,
+                    },
+                }],
+            })).to.eql({});
+        });
+
+        it('works with nested fields', () => {
+            expect(processQuery(NESTED_FIELDS, {
+                allowedFields: [{
+                    field: 'emails.sent',
+                    operators: {
+                        $gt: true,
+                    },
+                }],
+            })).to.eql(NESTED_FIELDS);
+        });
+    });
+
+    describe('$expr - top level operator', () => {
+        const EXPR_QUERY = {
+            $expr: {
+                $lt: ['$spent', '$received'],
+            },
+        };
+
+        it('filters $expr', () => {
+            expect(processQuery(EXPR_QUERY, {
+                allowedFields: [],
+                topLevelOperators: {
+                    $expr: false,
+                },
+            })).to.eql({});
+        });
+
+        it('allows $expr', () => {
+            expect(processQuery(EXPR_QUERY, {
+                allowedFields: [],
+                topLevelOperators: {
+                    $expr: true,
+                },
+            })).to.eql(EXPR_QUERY);
+        });
+    });
+
+    describe('multiple fields', () => {
+        const MULTIPLE_FIELDS_QUERY_1 = {
+            age: 18,
+            name: {$gt: 'K'},
+        };
+
+        it('works with multiple fields', () => {
+            expect(processQuery(MULTIPLE_FIELDS_QUERY_1, {
+                allowedFields: [{
+                    field: 'age',
+                    operators: {
+                        $eq: true,
+                    },
+                }, {
+                    field: 'name',
+                }],
+            })).to.eql(MULTIPLE_FIELDS_QUERY_1);
+        });
+
+        it('filters partially multiple fields', () => {
+            expect(processQuery(MULTIPLE_FIELDS_QUERY_1, {
+                allowedFields: [{
+                    field: 'age',
+                    operators: {
+                        $eq: true,
+                    },
+                }],
+            })).to.eql({age: 18});
+        });
+
+        it('filters all', () => {
+            expect(processQuery(MULTIPLE_FIELDS_QUERY_1, {
+                allowedFields: [{
+                    field: 'name',
+                    operators: {
+                        $eq: true,
+                    },
+                }],
+            })).to.eql({});
+        });
+
+        const MULTIPLE_FIELDS_QUERY_2 = {
+            ...MULTIPLE_FIELDS_QUERY_1,
+            $text: {$gt: ['$a', '$b']},
+        };
+
+        it('allows $text', () => {
+            expect(processQuery(MULTIPLE_FIELDS_QUERY_2, {
+                allowedFields: [{
+                    field: 'age',
+                    operators: {
+                        $eq: true,
+                    },
+                }, {
+                    field: 'name',
+                }],
+                topLevelOperators: {
+                    $text: true,
+                },
+            })).to.eql(MULTIPLE_FIELDS_QUERY_2);
+        });
+
+        it('filters $text', () => {
+            expect(processQuery(MULTIPLE_FIELDS_QUERY_2, {
+                allowedFields: [{
+                    field: 'age',
+                    operators: {
+                        $eq: true,
+                    },
+                }, {
+                    field: 'name',
+                }],
+                topLevelOperators: {
+                    $text: false,
+                },
+            })).to.eql(MULTIPLE_FIELDS_QUERY_1);
+        });
+    });
+});
+
+describe('processFilters - throw on Error', () => {
+    before(() => {
+        setDefaultConfig({
+            errorHandling: {
+                throw: true,
+            },
+        });
+    });
+
+    it('throws on forbidden field', () => {
+        expect(() => {
+            processQuery({age: 18}, {allowedFields: []});
+        }).to.throw(/Field age not allowed by schema/i);
+    });
+
+    it('throws on forbidden operator', () => {
+        expect(() => {
+            processQuery({age: 18}, {allowedFields: [{
+                field: 'age',
+                operators: {
+                    $eq: false,
+                },
+            }]});
+        }).to.throw(/Operator\(s\) \$eq for field age/i);
+    });
+
+    it('throws on $elemMatch', () => {
+        expect(() => {
+            processQuery({results: {$elemMatch: {age: 18}}}, {allowedFields: [{
+                field: 'results',
+                operators: {
+                    $elemMatch: true,
+                },
+            }]});
+        }).to.throw(/Field results\.age not allowed by schema/i);
+    });
+
+    it('throws on $where', () => {
+        expect(() => {
+            processQuery({$expr: {}}, {
+                allowedFields: [{
+                    field: 'results',
+                    operators: {
+                        $elemMatch: true,
+                    },
+                }],
+            });
+        }).to.throw(/Operator \$expr not allowed/i);
+    });
+
+    it('throws on top level array operators', () => {
+        expect(() => {
+            processQuery({
+                $or: [{age: {$gt: 18}}],
+            }, {
+                allowedFields: [],
+            });
+        }).to.throw(/Field age not allowed/i);
+    });
+});
+
+describe('processFilters - throw on Error', () => {
+    let onErrorCallback = stub();
+
+    beforeEach(() => {
+        setDefaultConfig({
+            errorHandling: {
+                onError: onErrorCallback,
+            },
+        });
+    });
+
+    it('works on forbidden field', () => {
+        processQuery({age: 18}, {allowedFields: []});
+
+        expect(onErrorCallback.callCount).to.equal(1);
+        
+        const [callArgs] = onErrorCallback.args;
+
+        expect(callArgs).to.have.length(1);
+        const [arg] = callArgs;
+        expect(arg.config).to.be.an('object');
+        expect(arg.message).to.equal('Field age not allowed by schema.');
+        expect(arg.context).to.be.an('object');
+        expect(arg.context.name).to.equal('age');
+        expect(arg.context.value).to.equal(18);
     });
 });
